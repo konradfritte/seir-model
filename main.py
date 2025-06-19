aad00b73103c6afc54bf58615d0b9799649df0db
Konrad Fritsche <konrad.fritsche@docyet.com>
2025-06-19 13:03:54 +0200

    add basic birth and death rate to seir model

--- main.py
+++ main.py
@@ -21,53 +21,63 @@ def euler_method(f, x0, t, h):
 # Alpha: Determines the transition rate from exposed to infectious people. Its reciprocal 1/alpha is the incubation time in days.
 # Beta: Determines the transmission rate from subsceptible to exposed people. It is usually connected to the reproduction number R0 and the recovery rate gamma.
 # Gamma: Determines the recovery rate from infectious to recovered people. Its reciprocal 1/gamma is the recovery time in days.
-def seir_model(alpha, beta, gamma):
+def seir_model(alpha, beta, gamma, ny=0, my=0):
     def f(t, x):
+        n, s, e, i, r = x
 
-        s, e, i, _ = x
+        dn = (ny - my) * n
+        ds = ny * n - 1 / n * beta(t) * s * i - my * s
+        de = 1 / n  * beta(t) * s * i - alpha * e - my * e
+        di = alpha * e - gamma * i - my * i
+        dr = gamma * i - my * r
 
-        ds = -beta(t) * s * i
-        de = beta(t) * s * i - alpha * e
-        di = alpha * e - gamma * i
-        dr = gamma * i
-
-        dx = np.array([ds, de, di, dr])
+        dx = np.array([dn, ds, de, di, dr])
 
         return dx
 
     return f
 
-def beta_modulator(beta, amplitude, phi, period):
+def beta_modulator(beta, amplitude=0, phi=0, period=365):
     def f(t):
         return beta * (1 + amplitude * np.sin(2 * np.pi * (t + phi) / period))
 
     return f
 
 def seir_simulation():
-    r0 = 2
-    alpha = 1/5
-    gamma = 1/10
-    beta = beta_modulator(r0 * gamma, 1, 365 / 4, 365)
+    r0 = 2.4
+
+    alpha = 1/5.5
+    gamma = 1/3
+    beta = beta_modulator(r0 * gamma)
 
     f = seir_model(alpha, beta, gamma)
 
-    s = 0.999
-    e = 1 - s
+    n = 83200000
+    s = n - 40000
+    e = n - s
     i = 0
     r = 0
-    x0 = np.array([s, e, i, r])
+    x0 = np.array([n, s, e, i, r])
 
-    t = 1 * 365
+    t = 365
     h = 1
 
     result = euler_method(f, x0, t, h)
     t, x = zip(*result)
+    _, s, e, i, r = zip(*x)
 
-    x = np.array(x)
-    labels = ["s", "e", "i", "r"]
 
-    plt.plot(t, x, label=labels)
-    plt.legend()
+    fig, ax = plt.subplots()
+
+
+    plt.grid(linestyle=":", color="lightgrey")
+
+    ax.plot(t, s, label="Subsceptible", color="blue")
+    ax.plot(t, e, label="Exposed", color="orange")
+    ax.plot(t, i, label="Infectious", color="red")
+    ax.plot(t, r, label="Recovered", color="green")
+
+    ax.legend()
     plt.show()
 
 seir_simulation()
