26b04d0033585204c72d9c960f2e02ae554afe75
Konrad Fritsche <konrad.fritsche@docyet.com>
2025-06-23 12:03:18 +0200

    First try of using a transition matrix instead of differential equations

--- model.py
+++ model.py
@@ -11,32 +11,119 @@ import numpy as np
 # Eta: Determines the transition rate from hospitalized to recovered compartment. Its reciprocal 1/eta is the recovery time for a hospitalized patient.
 # Ny: Determines the natural birth rate for the total population.
 # My: Determines the natural death rate for the total population.
+# def seihrsd_model(alpha, beta, gamma, delta, epsilon, epsilon_h, zeta, eta, ny, my):
+#     def f(t, x):
+#         n, s, e, i, ih, r, _, _, _, _, _, _, _ = x
+
+#         dn = (ny - my) * n - epsilon(t,ih) * ih - epsilon() * i
+
+#         ds = ny * n + delta() * r - 1 / n * beta(t) * s * i - my * s
+#         de = 1 / n  * beta(t) * s * i - alpha() * e - my * e
+#         di = alpha() * e - gamma() * i - zeta() * i - epsilon() * i - my * i
+#         dih = zeta() * i - eta() * ih - epsilon_h(t, ih) * ih - my * ih
+#         dr = gamma() * i + eta() * ih - delta() * r - my * r
+#         dd = epsilon_h(ih) * ih + epsilon() * i
+
+#         # Rates for cumulated cases
+#         dc_s = ny * n + delta() * r
+#         dc_e = 1 / n  * beta(t) * s * i
+#         dc_i = alpha() * e
+#         dc_ih = zeta() * i
+#         dc_r = eta() * ih + gamma() * i
+#         dc_d = epsilon_h(t, ih) * ih + epsilon() * i
+
+#         dx = np.array([dn, ds, de, di, dih, dr, dd, dc_s, dc_e, dc_i, dc_ih, dc_r, dc_d])
+
+#         return dx
+
+#     return f
+
 def seihrsd_model(alpha, beta, gamma, delta, epsilon, epsilon_h, zeta, eta, ny, my):
     def f(t, x):
-        n, s, e, i, ih, r, _, _, _, _, _, _, _ = x
+        n, s, e, i, ih, r, d, _, _, _, _, _, _ = x
 
-        dn = (ny - my) * n - epsilon(t,ih) * ih - epsilon() * i
+        p = [s, e, i, ih, r, d]
 
-        ds = ny * n + delta() * r - 1 / n * beta(t) * s * i - my * s
-        de = 1 / n  * beta(t) * s * i - alpha() * e - my * e
-        di = alpha() * e - gamma() * i - zeta() * i - epsilon() * i - my * i
-        dih = zeta() * i - eta() * ih - epsilon_h(t, ih) * ih - my * ih
-        dr = gamma() * i + eta() * ih - delta() * r - my * r
-        dd = epsilon_h(ih) * ih + epsilon() * i
+        w_ss = - 1 / n * beta(t) * i
+        w_se = 0
+        w_si = 0
+        w_sh = 0
+        w_sr = delta()
+        w_sd = 0
 
-        # Rates for cumulated cases
-        dc_s = ny * n + delta() * r
-        dc_e = 1 / n  * beta(t) * s * i
-        dc_i = alpha() * e
-        dc_ih = zeta() * i
-        dc_r = eta() * ih + gamma() * i
-        dc_d = epsilon_h(t, ih) * ih + epsilon() * i
+        w_es = 1 / n  * beta(t) * i
+        w_ee = - alpha()
+        w_ei = 0
+        w_eh = 0
+        w_er = 0
+        w_ed = 0
 
-        dx = np.array([dn, ds, de, di, dih, dr, dd, dc_s, dc_e, dc_i, dc_ih, dc_r, dc_d])
+        w_is = 0
+        w_ie = alpha()
+        w_ii = - gamma() - zeta() - epsilon()
+        w_ih = 0
+        w_ir = 0
+        w_id = 0
 
-        return dx
+        w_hs = 0
+        w_he = 0
+        w_hi = zeta()
+        w_hh = - eta() - epsilon_h(t, ih)
+        w_hr = 0
+        w_hd = 0
+
+        w_rs = 0
+        w_re = 0
+        w_ri = gamma()
+        w_rh = eta()
+        w_rr = - delta()
+        w_rd = 0
+
+        w_ds = 0
+        w_de = 0
+        w_di = epsilon()
+        w_dh = epsilon_h(t, ih)
+        w_dr = 0
+        w_dd = 0
+
+        q = np.array([
+            [w_ss, w_se, w_si, w_sh, w_sr, w_sd],
+            [w_es, w_ee, w_ei, w_eh, w_er, w_ed],
+            [w_is, w_ie, w_ii, w_ih, w_ir, w_id],
+            [w_hs, w_he, w_hi, w_hh, w_hr ,w_hd],
+            [w_rs, w_re, w_ri, w_rh, w_rr, w_rd],
+            [w_ds, w_de, w_di, w_dh, w_dr, w_dd]
+             ])
+
+        if int((np.sum(q))) != 0:
+            print(q)
+            print(np.sum(q, axis=0))
+            raise "Invalid Transition Matrix"
+
+        dx = np.dot(q, p)
+
+        # ds = ny * n + delta() * r - 1 / n * beta(t) * s * i - my * s
+        # de = 1 / n  * beta(t) * s * i - alpha() * e - my * e
+        # di = alpha() * e - gamma() * i - zeta() * i - epsilon() * i - my * i
+        # dih = zeta() * i - eta() * ih - epsilon_h(t, ih) * ih - my * ih
+        # dr = gamma() * i + eta() * ih - delta() * r - my * r
+        # dd = epsilon_h(ih) * ih + epsilon() * i
+
+        dn = 0
+
+        dc_s = 0
+        dc_e = 0
+        dc_i = 0
+        dc_h = 0
+        dc_r = 0
+        dc_d = 0
+
+        # dx = np.array([dn, ds, de, di, dih, dr, dd, dc_s, dc_e, dc_i, dc_ih, dc_r, dc_d])
+
+        return np.concatenate((np.array([dn]), dx, np.array([dc_s, dc_e, dc_i, dc_h, dc_r, dc_d])))
 
     return f
+
 
 # Moderator function for the alpha model parameter
 # Alpha: The transition rate from exposed to infectious compartment
